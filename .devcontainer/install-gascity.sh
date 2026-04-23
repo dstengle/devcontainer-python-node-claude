8644f913a7cbfa12f7adc928fb56812853eedbbb
David Stenglein <dave@missingmass.com>
2026-04-23 19:30:11 +0000

    Apr 23, 2026, 3:30 PM

---
+++ .devcontainer/install-gascity.sh
@@ -0,0 +1,20 @@
+# Set the version you want (check https://github.com/gastownhall/gascity/releases)
+VERSION=0.13.3
+
+# Detect platform
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+case "$ARCH" in
+  x86_64)         ARCH=amd64 ;;
+  aarch64|arm64)  ARCH=arm64 ;;
+esac
+
+# Download and extract
+curl -fsSLO "https://github.com/gastownhall/gascity/releases/download/v${VERSION}/gascity_${VERSION}_${OS}_${ARCH}.tar.gz"
+tar -xzf "gascity_${VERSION}_${OS}_${ARCH}.tar.gz"
+
+# Move to a directory on your PATH
+sudo install -m 755 gc /usr/local/bin/gc
+
+# Verify
+gc version
